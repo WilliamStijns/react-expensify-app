b86d871040458610b0ae87431c4f0943d7617013
William Stijns <william.stijns@ziggo.nl>
2019-05-05 13:45:06 +0200

    Removed dummy espense data

--- src/app.js
+++ src/app.js
@@ -13,15 +13,15 @@ const store = configureStore();
 
 store.subscribe (()=> {
   const state = store.getState();
-  console.log(state);
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-  console.log("Visible:",visibleExpenses, state.filters);
+  // console.log(state);
+  // const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+  // console.log("Visible:",visibleExpenses, state.filters);
 });
 
 
-store.dispatch(addExpense({description: "Water bill", amount : 4500}));
-store.dispatch(addExpense({description: "Gas bill", createAt: 1000}));
-store.dispatch(addExpense({description: "Rent", amount : 109500, createAt: 100}));
+// store.dispatch(addExpense({description: "Water bill", amount : 4500}));
+// store.dispatch(addExpense({description: "Gas bill", createAt: 1000}));
+// store.dispatch(addExpense({description: "Rent", amount : 109500, createAt: 100}));
 
 // store.dispatch(setTextFilter('gas'));
 
